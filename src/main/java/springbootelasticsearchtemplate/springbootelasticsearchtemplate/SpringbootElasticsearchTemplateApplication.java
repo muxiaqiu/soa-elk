0f4951e85989e4c0089d2f7a876109be1da66d2b
qiumuxia6688 <49407965eclx>
2020-01-13 16:44:29 +0800

    提交源代码--邱慕夏--2020年1月13日16:29:25

---
+++ src/main/java/springbootelasticsearchtemplate/springbootelasticsearchtemplate/SpringbootElasticsearchTemplateApplication.java
@@ -0,0 +1,13 @@
+package springbootelasticsearchtemplate.springbootelasticsearchtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticsearchTemplateApplication.class, args);
+	}
+
+}
